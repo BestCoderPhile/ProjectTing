6b93e929e2e36a9caddc726b0e4af85c77dd116e
BestCoderPhile <46857637+BestCoderPhile@users.noreply.github.com>
2024-09-03 18:18:20 -0700

    Update website.js

--- website.js
+++ website.js
@@ -1,1 +1,17 @@
-hi skibidi
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <h1>
+        Andrew Hu
+    </h1>
+    <button onClick="alertButton()">click on me</button>
+    <script src="script.js" defer></script>
+
+</body>
+
+</html>
